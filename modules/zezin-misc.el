069013416e9606110f6f6e46cce8c15bddef1559
Guilherme Henrique <gjhenrique@gmail.com>
2017-02-04 00:29:07 -0200

    Add restclient package

--- modules/zezin-misc.el
+++ modules/zezin-misc.el
@@ -2,5 +2,24 @@
 
 (use-package nginx-mode)
 
+
+;; restclient
+(use-package restclient
+  :config
+  (zezin-add-keybinding :language
+			'(("f" restclient-http-send-current))
+			:map 'restclient-mode-map))
+
+(use-package company-restclient
+  :config
+  (add-to-list 'company-backends 'company-restclient))
+
+(use-package ob-restclient
+  :after org-plus-contrib
+  :config
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((restclient . t))))
+
 (provide 'zezin-misc)
 ;;; zezin-misc ends here
