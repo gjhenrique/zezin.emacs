4a32681503a5d887b31da3f44ad81675a3149ac5
Guilherme Henrique <cod.guilherme.jose@locaweb.com.br>
2017-03-29 13:25:47 -0300

    Fix js2 offset

--- modules/zezin-javascript.el
+++ modules/zezin-javascript.el
@@ -2,7 +2,7 @@
   :init
   (progn
     (setq-default js2-strict-trailing-comma-warning nil)
-    (setq js2-basic-offset 2)
+    (setq-default js2-basic-offset 2)
     (add-hook 'js2-mode-hook 'js2-imenu-extras-mode)
     (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
     (add-to-list 'auto-mode-alist '("\\.es6\\'" . js2-mode))))
