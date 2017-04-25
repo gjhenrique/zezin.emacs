a9277ad03c9985d77502a77d07a71b3ead3112e9
Guilherme Henrique <gjhenrique@gmail.com>
2017-04-24 22:58:16 -0300

    Open dired file in the same buffer

--- modules/zezin-purpose.el
+++ modules/zezin-purpose.el
@@ -4,6 +4,7 @@
   (add-to-list 'purpose-user-mode-purposes '(help-mode . help))
   (add-to-list 'purpose-user-mode-purposes '(web-mode . web))
   (add-to-list 'purpose-user-mode-purposes '(emacs-lisp-mode . elisp))
+  ;; TODO: This is ugly
   (add-to-list 'purpose-user-mode-purposes '(yaml-mode . ruby)))
 
 (use-package window-purpose
@@ -14,7 +15,13 @@
     (purpose-x-kill-setup)
     (zezin-add-purposes)
     (add-to-list 'global-mode-string '(:eval (purpose--modeline-string)))
-    (purpose-compile-user-configuration)))
+    (purpose-compile-user-configuration)
+					;
+    (defalias 'dired-find-file-without-purpose
+      (without-purpose-command #'dired-find-file))
+
+    (with-eval-after-load 'dired
+      (define-key dired-mode-map [remap dired-find-file] #'dired-find-file-without-purpose))))
 
 (use-package ivy-purpose)
 
