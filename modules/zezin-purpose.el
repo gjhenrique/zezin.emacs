da3202addcc8729917b302c4c708942ca3383077
Guilherme Henrique <gjhenrique@gmail.com>
2017-04-26 18:40:57 -0300

    Add reusable-frames for compilation and magit buffers

--- modules/zezin-purpose.el
+++ modules/zezin-purpose.el
@@ -1,27 +1,45 @@
 (defun zezin-add-purposes ()
   (add-to-list 'purpose-user-mode-purposes '(ruby-mode . ruby))
-  (add-to-list 'purpose-user-mode-purposes '(rspec-mode . rspec))
+  (add-to-list 'purpose-user-mode-purposes '(rspec-mode . spec))
   (add-to-list 'purpose-user-mode-purposes '(help-mode . help))
   (add-to-list 'purpose-user-mode-purposes '(web-mode . web))
   (add-to-list 'purpose-user-mode-purposes '(emacs-lisp-mode . elisp))
   ;; TODO: This is ugly
   (add-to-list 'purpose-user-mode-purposes '(yaml-mode . ruby)))
 
+(defun zezin-disable-dired ()
+  (defalias 'dired-find-file-without-purpose
+    (without-purpose-command #'dired-find-file))
+
+  (with-eval-after-load 'dired
+    (define-key dired-mode-map [remap dired-find-file] #'dired-find-file-without-purpose)))
+
+(defun zezin-add-reusable-buffers
+    (buffer-regex)
+  (add-to-list 'display-buffer-alist
+	       '(buffer-regex
+		 nil
+		 (reusable-frames . t))))
+
 (use-package window-purpose
+  :init
+  (progn
+    (setq purpose-default-action-order 'prefer-other-window)
+    (add-to-list 'display-buffer-alist
+		 '("." nil (reusable-frames . t))))
   :config
   (progn
     (purpose-mode)
     (purpose-x-golden-ratio-setup)
     (purpose-x-kill-setup)
-    (zezin-add-purposes)
-    (add-to-list 'global-mode-string '(:eval (purpose--modeline-string)))
-    (purpose-compile-user-configuration)
-					;
-    (defalias 'dired-find-file-without-purpose
-      (without-purpose-command #'dired-find-file))
+    (purpose-x-magit-single-on)
 
-    (with-eval-after-load 'dired
-      (define-key dired-mode-map [remap dired-find-file] #'dired-find-file-without-purpose))))
+    (zezin-add-reusable-buffers "\\**compilation\\*")
+    (zezin-add-reusable-buffers "\\*magit*")
+    (zezin-add-purposes)
+    (zezin-disable-dired)
+    (add-to-list 'global-mode-string '(:eval (purpose--modeline-string)))
+    (purpose-compile-user-configuration)))
 
 (use-package ivy-purpose)
 
