a791d846b4c6fbbdfd4d22d412a833347b6f3b1f
Guilherme Henrique <gjhenrique@gmail.com>
2017-05-30 13:52:20 -0300

    Add company-anaconda

--- modules/zezin-python.el
+++ modules/zezin-python.el
@@ -2,5 +2,10 @@
   :config
   (add-hook 'python-mode-hook 'anaconda-mode))
 
+(use-package company-anaconda
+  :after (anaconda company)
+  :config
+  (push 'company-anaconda company-backends ))
+
 (provide 'zezin-python)
 ;;; zezin-python.el ends here
