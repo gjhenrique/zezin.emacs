f0956641ed5afd2fb8789659b9743a4fe60a38ab
Guilherme Henrique <gjhenrique@gmail.com>
2017-06-09 14:49:48 -0300

    Add js-indent-level 2 to web-mode

--- modules/zezin-web.el
+++ modules/zezin-web.el
@@ -4,6 +4,7 @@
 	 ("\\.erb\\'" . web-mode))
   :init
   (setq-default css-indent-offset 2
+		js-indent-level 2
 		web-mode-markup-indent-offset 2
 		web-mode-css-indent-offset 2
 		web-mode-code-indent-offset 2
