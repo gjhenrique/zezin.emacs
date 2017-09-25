75f15362ae3187cf7fcaf5c8eb3ca53dced89ca2
Guilherme Henrique <gjhenrique@gmail.com>
2017-09-25 17:49:47 -0300

    Add flycheck-credo and set location of elixir source

--- modules/zezin-elixir.el
+++ modules/zezin-elixir.el
@@ -1,6 +1,18 @@
 (use-package elixir-mode)
 
-(use-package alchemist)
+(use-package alchemist
+  :init
+  (let ((elixir-project (substitute-in-file-name "$HOME/Projects/elixir/")))
+    (when (file-exists-p elixir-project)
+      (setq alchemist-goto-elixir-source-dir elixir-project))))
+
+(use-package flycheck-credo
+  :defer t
+  :init
+  (progn
+    (eval-after-load 'flycheck
+      '(flycheck-credo-setup))
+    (add-hook 'elixir-mode-hook 'flycheck-mode)))
 
 (with-eval-after-load 'smartparens
   (sp-with-modes '(elixir-mode)
