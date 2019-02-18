6336e6985b3833aa0006f812902e057a0b63307c
Guilherme Henrique <gjhenrique@gmail.com>
2019-02-18 12:26:28 +0100

    workaround to export posts with doom-theme

--- modules/zezin-blogging.el
+++ modules/zezin-blogging.el
@@ -3,6 +3,9 @@
   (org-babel-do-load-languages
    'org-babel-load-languages
    '((dot . t)))
+  ;; Fix invalid face hl-line error
+  (require 'hl-line)
+  (set-face-background 'hl-line "gray97")
   (defun my-org-confirm-babel-evaluate (lang body)
     (not (string= lang "dot")))
   (setq org-confirm-babel-evaluate 'my-org-confirm-babel-evaluate)
