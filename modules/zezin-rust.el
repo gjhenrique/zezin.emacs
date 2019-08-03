308e812f29d7ab7186e5e2811278ae1445f24ec9
Guilherme Henrique <gjhenrique@gmail.com>
2019-08-04 00:57:40 +0200

    serious rust configuration

--- modules/zezin-rust.el
+++ modules/zezin-rust.el
@@ -1,8 +1,28 @@
 (use-package racer
-  :mode "\\.rs\\'")
+  :hook (rust-mode . racer-mode))
 
 (use-package rust-mode
-  :mode "\\.rs\\'")
+  :mode "\\.rs\\'"
+  :init
+  (setq rust-format-on-save t))
+
+(use-package cargo
+  :after rust-mode
+  :hook (rust-mode . cargo-minor-mode))
+
+(use-package flycheck-rust
+  :commands flycheck-rust-setup
+  :after (flycheck rust-mode)
+  :hook ((flycheck-mode . flycheck-rust-setup)))
+
+(use-package cargo
+  :hook (rust-mode . cargo-minor-mode))
+
+(zezin-add-keybinding :language
+                      `(("f" racer-find-definition)
+                        ("j" racer-describe)
+                        ("g" racer-describe-tooltip))
+                      :map 'rust-mode-map)
 
 (provide 'zezin-rust)
 ;;; zezin-rust ends here
