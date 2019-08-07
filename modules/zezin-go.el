d603ca94c05ee2ebf2cba9acb7610a91e0f1caaa
Guilherme Henrique <gjhenrique@gmail.com>
2019-08-07 21:23:13 +0200

    putting go get

--- modules/zezin-go.el
+++ modules/zezin-go.el
@@ -1,3 +1,10 @@
+;; go get -u -v golang.org/x/tools/cmd/guru
+;; go get -u github.com/nsf/gocode
+;; go get -u github.com/rogpeppe/godef
+;; go get -u golang.org/x/lint/golint
+;; go get -u github.com/mdempsky/unconvert
+;; go get -u honnef.co/go/tools/cmd/staticcheck
+
 (use-package go-mode
   :mode ("\\.go\\'" . go-mode)
   :config
@@ -11,6 +18,9 @@
 
 (use-package go-guru
   :after go-mode)
+
+(use-package go-eldoc
+  :hook (go-mode . go-eldoc-setup))
 
 (zezin-add-keybinding :language
                       `(("f" go-guru-definition))
